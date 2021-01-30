2e18643365cc6ee39d4e4f6912d16a37ff053c9c
kfcpop <kfcpop2112@gmail.com>
2021-01-30 11:14:16 -0600

    SpinePlayerMovement

---
+++ IveGotYouSon/Assets/Resources/Scripts/SpinePlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+using Spine;
+using UnityEngine.UIElements;
+
+public class SpinePlayerMovement : MonoBehaviour
+{
+    [SpineAnimation]
+    public string idleAnimationName;
+    [SpineAnimation]
+    public string walkingAnimationName;
+    [SpineAnimation]
+    public string hitBackAnimationName;
+
+    public Spine.AnimationState spineAnimationState;
+    public SkeletonAnimation skeletonAnimation;
+    public Spine.Skeleton skeleton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        spineAnimationState = skeletonAnimation.AnimationState;
+        skeleton = skeletonAnimation.Skeleton;
+        spineAnimationState.SetAnimation(0, walkingAnimationName, true, );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
