9cac6a247effe5925c80458dbe5ce66e30de6bf4
Michael Boal <mgb519@aol.com>
2021-01-30 12:56:46 -0600

    Created Sprite Sorter

---
+++ IveGotYouSon/Assets/Resources/Scripts/SpriteSorter.cs
@@ -0,0 +1,17 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    SpriteRenderer sprite;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        sprite.sortingOrder = (int)(transform.position.y*1000);
+    }
+}
