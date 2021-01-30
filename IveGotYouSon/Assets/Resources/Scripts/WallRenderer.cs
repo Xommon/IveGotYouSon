f09228746b2929b1c034d65e983a56eea40227be
Michael Boal <mgb519@aol.com>
2021-01-30 12:46:11 -0600

    Created Wall Renderer Script

---
+++ IveGotYouSon/Assets/Resources/Scripts/WallRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+
+public class WallRenderer : MonoBehaviour {
+    public GameObject[] walls;
+    List<SkeletonRenderer> wallSprites;
+    bool playerPresent;
+
+    
+
+
+
+    void Start() {
+        wallSprites = new List<SkeletonRenderer>();
+        foreach (GameObject wall in walls) {
+            wallSprites.Add(wall.GetComponent<SkeletonRenderer>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        foreach (SkeletonRenderer sprite in wallSprites)
+        {
+            sprite.enabled = playerPresent;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            playerPresent = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            playerPresent = false;
+        }
+    }
+}
