9417635051174086ffd151730f792638b4bfc3d7
Michael Boal <mgb519@aol.com>
2021-01-30 14:42:19 -0600

    Updated Sprite Sorter

--- IveGotYouSon/Assets/Resources/Scripts/SpriteSorter.cs
+++ IveGotYouSon/Assets/Resources/Scripts/SpriteSorter.cs
@@ -3,15 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteSorter : MonoBehaviour
-{
-    SpriteRenderer sprite;
+public class SpriteSorter : MonoBehaviour {
+
+    public SpriteRenderer sprite;
+    public int sort;
+    static Vector2 xTranslation = new Vector2(Mathf.Sqrt(2) / 2, Mathf.Sqrt(2) / 2);
+    static Vector2 yTranslation = new Vector2(-Mathf.Sqrt(2) / 2, Mathf.Sqrt(2) / 2);
 
     void Start() {
         sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
-        sprite.sortingOrder = (int)(transform.position.y*1000);
+        sort = (int)((transform.position.x* xTranslation +transform.position.y * yTranslation).x * 10000);
+        print(sort);
+        sprite.sortingOrder = sort;
     }
 }
