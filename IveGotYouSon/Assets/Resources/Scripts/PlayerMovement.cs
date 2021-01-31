07461ea18f843139a815d94dd98528f80414d6db
Micheal Quentin <mquenti3@gmail.com>
2021-01-31 02:48:27 -0600

    Revert "Update PlayerMovement.cs"
    This reverts commit cd07100d96e68c38b91b150b6bbff6c87c5d394d.

--- IveGotYouSon/Assets/Resources/Scripts/PlayerMovement.cs
+++ IveGotYouSon/Assets/Resources/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -56,11 +57,11 @@ public class PlayerMovement : MonoBehaviour
         {
             if (health - 1 < i)
             {
-                hearts[i].GetComponent<Image>().sprite = heartSprites[1];
+                //hearts[i].GetComponent<Image>().sprite = heartSprites[1];
             }
             else
             {
-                hearts[i].GetComponent<Image>().sprite = heartSprites[0];
+                //hearts[i].GetComponent<Image>().sprite = heartSprites[0];
             }
         }
 
@@ -73,11 +74,11 @@ public class PlayerMovement : MonoBehaviour
         {
             if (bearCount - 1 < i)
             {
-                bears[i].GetComponent<Image>().sprite = bearSprites[1];
+                //bears[i].GetComponent<Image>().sprite = bearSprites[1];
             }
             else
             {
-                bears[i].GetComponent<Image>().sprite = bearSprites[0];
+                //bears[i].GetComponent<Image>().sprite = bearSprites[0];
             }
         }
 
