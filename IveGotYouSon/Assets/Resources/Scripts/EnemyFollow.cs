dd2cfb3bfa247bd9e30314e8040ae269f184cc1e
Micheal Quentin <mquenti3@gmail.com>
2021-01-30 20:28:15 -0600

    Player Can Throw Projectiles
    They hit enemies and kill them.

--- IveGotYouSon/Assets/Resources/Scripts/EnemyFollow.cs
+++ IveGotYouSon/Assets/Resources/Scripts/EnemyFollow.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    public int health;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,24 @@ public class EnemyFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health < 1)
+        {
+            speed = 0;
+            transform.localScale -= new Vector3(0.1f, 0.1f, 0);
+        }
+
+        if (transform.localScale.x <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Pillow")
+        {
+            health -= 1;
+            Destroy(collision.gameObject);
+        }
     }
 }
