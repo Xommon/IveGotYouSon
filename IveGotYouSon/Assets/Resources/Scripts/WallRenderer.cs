7e660eeb8d1463525963909bc4bb34c0391d6852
Michael Boal <mgb519@aol.com>
2021-01-30 20:28:01 -0600

    Updated WallRenderer

--- IveGotYouSon/Assets/Resources/Scripts/WallRenderer.cs
+++ IveGotYouSon/Assets/Resources/Scripts/WallRenderer.cs
@@ -7,6 +7,7 @@ public class WallRenderer : MonoBehaviour {
     public List<SpriteRenderer> wallSprites;
     public SpriteRenderer floor;
     public bool playerPresent;
+    public GameObject player;
     float top, bottom, left, right;
 
     void Start() {
@@ -18,6 +19,7 @@ public class WallRenderer : MonoBehaviour {
         bottom = transform.position.y + 10;
         left = transform.position.x - 10;
         right = transform.position.x + 10;
+        player = GameObject.FindGameObjectWithTag("player");
     }
 
     // Update is called once per frame
@@ -30,7 +32,7 @@ public class WallRenderer : MonoBehaviour {
     private void OnTriggerStay2D(Collider2D other) {
         float x = other.transform.position.x;
         float y = other.transform.position.y;
-        if (x < left || x >= right || y < top || y >= bottom) {
+        if (x < left && x >= right && y < top && y >= bottom) {
             playerPresent = false;
         }
         else {
